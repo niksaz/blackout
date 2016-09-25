50676e2820c174b288c983b308677359e8f00a7b
niksaz <nikitasazanovich@gmail.com>
2016-09-25 13:23:24 +0300

    basic MainMenu

---
+++ core/src/ru/spbau/blackout/utils/Constants.java
@@ -0,0 +1,20 @@
+package ru.spbau.blackout.utils;
+
+import com.badlogic.gdx.graphics.Color;
+
+public class Constants {
+    public static final String BLACKOUT_TEXT = "Blackout";
+
+    public static final int VIRTUAL_WORLD_WIDTH = 800;
+    public static final int VIRTUAL_WORLD_HEIGHT = 480;
+
+    public static final Color MAIN_MENU_BACKGROUND_COLOR = new Color(0.1f, 0.1f, 0.1f, 1.0f);
+    public static final Color MAIN_MENU_BLACKOUT_LABEL_COLOR = Color.WHITE;
+    public static final float MAIN_MENU_BUTTON_PADDING = 10.f;
+    public static final float MAIN_MENU_BLACKOUT_LABEL_BOTTOM_PADDING = 25.0f;
+    public static final float MAIN_MENU_BLACKOUT_LABEL_SCALE = 2.0f;
+    public static final String MAIN_MENU_BUTTON_PATH = "button.png";
+    public static final String MAIN_MENU_BUTTON_PLAY_TEXT = "Play";
+    public static final String MAIN_MENU_BUTTON_SHOP_TEXT = "Shop";
+    public static final String MAIN_MENU_BUTTON_LEADERBOARD_TEXT = "Leaderboard";
+}
