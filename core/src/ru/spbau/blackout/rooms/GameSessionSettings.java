c191de3ff9b00db451a909c22456a204adcf62fc
Andrey Tonkikh <xosmig@gmail.com>
2016-11-22 00:48:54 +0300

    GameObject serializable fix

--- core/src/ru/spbau/blackout/rooms/GameSessionSettings.java
+++ core/src/ru/spbau/blackout/rooms/GameSessionSettings.java
@@ -12,7 +12,21 @@ import ru.spbau.blackout.entities.Hero;
  * Contains settings for particular game session.
  */
 public interface GameSessionSettings extends Serializable {
+
+    // FIXME: map is more complicated thing than just a model
+    /**
+     * Returns a path to the map model.
+     */
     String getMap();
+
+    /**
+     * Returns a list of definitions of all GameObjects in the game.
+     */
     List<GameObject.Definition> getObjectDefs();
+
+    /**
+     * Returns a definition of current player's character.
+     * Note that it also must be represented in the common list of GameObjects
+     */
     Hero.Definition getCharacter();
 }
