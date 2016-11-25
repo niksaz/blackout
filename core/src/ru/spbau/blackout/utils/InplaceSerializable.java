46f3ad706b900156d6136d68436e88a486c12735
niksaz <nikitasazanovich@gmail.com>
2016-11-25 20:56:59 +0300

    gameWorld refactoring due to deserializable 2

--- core/src/ru/spbau/blackout/utils/InplaceSerializable.java
+++ core/src/ru/spbau/blackout/utils/InplaceSerializable.java
@@ -1,5 +1,6 @@
 package ru.spbau.blackout.utils;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
@@ -7,7 +8,8 @@ public interface InplaceSerializable {
     /**
      * Ignores inplaceDeserializeImpl return value.
      */
-    static void inplaceDeserialize(InplaceSerializable dest, ObjectInputStream in) {
+    static void inplaceDeserialize(InplaceSerializable dest, ObjectInputStream in)
+            throws IOException, ClassNotFoundException {
         dest.inplaceDeserializeImpl(in);
     }
 
@@ -15,14 +17,15 @@ public interface InplaceSerializable {
      * Same as <code>source.inplaceSerializeImpl()</code>, but highly preferred
      * because it looks similar to <code>inplaceDeserialize(dest, in)</code>.
      */
-    static void inplaceSerialize(InplaceSerializable source, ObjectOutputStream out) {
+    static void inplaceSerialize(InplaceSerializable source, ObjectOutputStream out)
+            throws IOException, ClassNotFoundException {
         source.inpaceSerializeImpl(out);
     }
 
     /**
      * Opposite to <code>inplaceDeserializeImpl</code>
      */
-    void inpaceSerializeImpl(ObjectOutputStream out);
+    void inpaceSerializeImpl(ObjectOutputStream out) throws IOException, ClassNotFoundException;
 
     /**
      * Opposite to <code>inplaceSerializeImpl</code>
@@ -30,5 +33,5 @@ public interface InplaceSerializable {
      * Returns any data which is necessary for deserialization of derived classes.
      * (look at <code>GameObject</code> deserialization, to see when the return value is necessary)
      */
-    Object inplaceDeserializeImpl(ObjectInputStream in);
+    Object inplaceDeserializeImpl(ObjectInputStream in) throws IOException, ClassNotFoundException;
 }
