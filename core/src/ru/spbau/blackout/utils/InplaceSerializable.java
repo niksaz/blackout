f43977b7ee946f134daa04d875670b79779fddba
Andrey Tonkikh <xosmig@gmail.com>
2016-11-25 20:47:56 +0300

    comments for InplaceSerializable

--- core/src/ru/spbau/blackout/utils/InplaceSerializable.java
+++ core/src/ru/spbau/blackout/utils/InplaceSerializable.java
@@ -7,7 +7,6 @@ import java.io.ObjectOutputStream;
 public interface InplaceSerializable {
     /**
      * Ignores inplaceDeserializeImpl return value.
-     * Don't override this method.
      */
     static void inplaceDeserialize(InplaceSerializable dest, ObjectInputStream in)
         throws IOException, ClassNotFoundException
@@ -15,17 +14,26 @@ public interface InplaceSerializable {
         dest.inplaceDeserializeImpl(in);
     }
 
+    /**
+     * Same as <code>source.inplaceSerializeImpl()</code>, but highly preferred
+     * because it looks similar to <code>inplaceDeserialize(dest, in)</code>.
+     */
     static void inplaceSerialize(InplaceSerializable source, ObjectOutputStream out)
             throws IOException, ClassNotFoundException
     {
         source.inpaceSerializeImpl(out);
     }
 
+    /**
+     * Opposite to <code>inplaceDeserializeImpl</code>
+     */
     void inpaceSerializeImpl(ObjectOutputStream out) throws IOException, ClassNotFoundException;
 
     /**
+     * Opposite to <code>inplaceSerializeImpl</code>
      * Used to do inplace deserialization.
-     * Returns any data which is necessary for derived classes.
+     * Returns any data which is necessary for deserialization of derived classes.
+     * (look at <code>GameObject</code> deserialization, to see when the return value is necessary)
      */
     Object inplaceDeserializeImpl(ObjectInputStream in) throws IOException, ClassNotFoundException;
 }
