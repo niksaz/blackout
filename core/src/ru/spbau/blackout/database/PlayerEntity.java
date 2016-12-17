b987eff606b41d59cc2e330d0ba5e381627746ae
niksaz <nikitasazanovich@gmail.com>
2016-12-17 17:01:10 +0300

    player's character stored in the database

--- core/src/ru/spbau/blackout/database/PlayerEntity.java
+++ core/src/ru/spbau/blackout/database/PlayerEntity.java
@@ -3,7 +3,15 @@ package ru.spbau.blackout.database;
 import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Id;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+
+import ru.spbau.blackout.abilities.Ability;
+import ru.spbau.blackout.abilities.fireball.FireballAbility;
+import ru.spbau.blackout.entities.Character;
+import ru.spbau.blackout.shapescreators.CircleCreator;
 
 /**
  * Representation of a player profile in a database.
@@ -14,18 +22,33 @@ public class PlayerEntity implements Serializable {
     @Id
     private String name;
     private int gold;
+    private byte[] serializedDefinition;
 
     public PlayerEntity() {
         this("");
     }
 
     public PlayerEntity(String name) {
-        this(name, 0);
-    }
-
-    public PlayerEntity(String name, int gold) {
         this.name = name;
-        this.gold = gold;
+
+        final Character.Definition initialCharacter = new Character.Definition(
+                "models/wizard/wizard.g3db",
+                new CircleCreator(0.6f),
+                0, 0,
+                new Ability[] { new FireballAbility(7) },
+                200
+        );
+        try (
+            ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(byteOutput)
+        ) {
+            out.writeObject(initialCharacter);
+            out.flush();
+            serializedDefinition = byteOutput.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IllegalStateException();
+        }
     }
 
     public PlayerEntity(PlayerEntity other) {
@@ -39,6 +62,10 @@ public class PlayerEntity implements Serializable {
 
     public int getGold() {
         return gold;
+    }
+
+    public byte[] getSerializedDefinition() {
+        return serializedDefinition;
     }
 
     public void changeGold(int delta) {
---
+++ server/src/main/java/ru/spbau/blackout/serverside/database/DatabaseAccessor.java
@@ -0,0 +1,31 @@
+package ru.spbau.blackout.serverside.database;
+
+import com.mongodb.MongoClient;
+
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+import ru.spbau.blackout.database.Database;
+import ru.spbau.blackout.database.PlayerEntity;
+
+public class DatabaseAccessor {
+
+    private static final DatabaseAccessor instance = new DatabaseAccessor();
+
+    private Datastore datastore;
+
+    private DatabaseAccessor() {
+        final Morphia morphia = new Morphia();
+        morphia.map(PlayerEntity.class);
+        datastore = morphia.createDatastore(new MongoClient(), Database.DATABASE_NAME);
+        datastore.ensureIndexes();
+    }
+
+    public static DatabaseAccessor getInstance() {
+        return instance;
+    }
+
+    public Datastore getDatastore() {
+        return datastore;
+    }
+}
