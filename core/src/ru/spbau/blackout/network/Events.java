d7843b56612aeaa9263feb743d530843d0bbb3a2
Andrey Tonkikh <xosmig@gmail.com>
2016-12-18 21:55:27 +0300

    Events added

---
+++ core/src/ru/spbau/blackout/network/Events.java
@@ -0,0 +1,10 @@
+package ru.spbau.blackout.network;
+
+import ru.spbau.blackout.entities.GameUnit;
+
+public class Events {
+
+    void abilityCast(GameUnit unit, int abilityNum) {
+        // TODO: I will make this function
+    }
+}
